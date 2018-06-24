7d49cb482f5271041a7eef8018abb7cd08a03e25
Aleksey <lesha.leshka@inbox.ru>
2018-06-24 14:12:55 +0300

    Chain seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-
-        $this->call(UsersSeeder::class);
-        $this->call(SampleDataSeeder::class);
+        $this
+            ->call(UsersSeeder::class)
+            ->call(SampleDataSeeder::class);
     }
 }
