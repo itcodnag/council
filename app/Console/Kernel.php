34899baa610f9e4c9852488a5da5a5c74f19897b
Aleksey <lesha.leshka@inbox.ru>
2018-06-25 14:46:54 +0300

    Installation command - initial setup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,19 +7,20 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
     /**
      * The Artisan commands provided by your application.
      *
      * @var array
      */
     protected $commands = [
-        //
+        Commands\InstallCommand::class
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
