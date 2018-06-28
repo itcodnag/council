ee33a9c0637fcc78f07de2fdec6668442979e514
Aleksey <lesha.leshka@inbox.ru>
2018-06-28 16:40:40 +0300

    Refactor a couple things

--- app/Events/ThreadReceivedNewReply.php
+++ app/Events/ThreadReceivedNewReply.php
@@ -9,15 +9,29 @@ class ThreadReceivedNewReply
 {
     use Dispatchable, SerializesModels;
 
+    /**
+     * The reply that was posted.
+     *
+     * @param \App\Reply $reply
+     */
     public $reply;
 
     /**
      * Create a new event instance.
      *
-     * @param $reply
+     * @param $reply \App\Reply $reply
      */
     public function __construct($reply)
     {
         $this->reply = $reply;
+    }
+
+    /**
+     * Get the subject of the event.
+     * @return \App\Reply
+     */
+    public function subject()
+    {
+        return $this->reply;
     }
 }
