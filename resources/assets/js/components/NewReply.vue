fba81377b6533e8e14b02ef3f19f21e50a675e86
Aleksey <lesha.leshka@inbox.ru>
2018-06-29 14:35:06 +0300

    Don't show new reply form if not confirmed

--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
-        <div v-if="signedIn">
+        <div v-if="! signedIn">
+            <p class="text-center">
+                Please <a href="/login">sign in</a> to participate in this
+                discussion.
+            </p>
+        </div>
+
+        <div v-else-if="! confirmed">
+            To participate in this thread, please check your email and confirm your account.
+        </div>
+
+        <div v-else>
             <div class="form-group">
                 <wysiwyg name="body" v-model="body" placeholder="Have something to say?"></wysiwyg>
             </div>
 
             <button type="submit"
                     class="btn btn-default"
-                    @click="addReply">Post</button>
+                    @click="addReply">Post
+            </button>
         </div>
-
-        <p class="text-center" v-else>
-            Please <a href="/login">sign in</a> to participate in this
-            discussion.
-        </p>
     </div>
 </template>
 
@@ -29,13 +36,19 @@
             };
         },
 
+        computed: {
+            confirmed() {
+                return window.App.user.confirmed;
+            }
+        },
+
         mounted() {
             $('#body').atwho({
                 at: "@",
                 delay: 750,
                 callbacks: {
-                    remoteFilter: function(query, callback) {
-                        $.getJSON("/api/users", {name: query}, function(usernames) {
+                    remoteFilter: function (query, callback) {
+                        $.getJSON("/api/users", {name: query}, function (usernames) {
                             callback(usernames)
                         });
                     }
@@ -45,7 +58,7 @@
 
         methods: {
             addReply() {
-                axios.post(location.pathname + '/replies', { body: this.body })
+                axios.post(location.pathname + '/replies', {body: this.body})
                     .catch(error => {
                         flash(error.response.data, 'danger');
                     })
