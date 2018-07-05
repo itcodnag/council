bf3d9dbf016f621db00baf8d238ba442d75377af
Aleksey <lesha.leshka@inbox.ru>
2018-07-05 14:44:15 +0300

    Use global scope for active channel results

--- app/Channel.php
+++ app/Channel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
+
     /**
      * The attributes that aren't mass assignable.
      *
@@ -19,6 +20,16 @@ class Channel extends Model
     protected $casts = [
         'archived' => 'boolean'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('active', function ($builder) {
+            $builder->where('archived', false)
+                ->orderBy('name', 'asc');
+        });
+    }
 
     /**
      * Get the route key name for Laravel.
--- tests/Unit/ChannelTest.php
+++ tests/Unit/ChannelTest.php
@@ -2,13 +2,15 @@
 
 namespace Tests\Feature;
 
+use App\Channel;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ChannelTest extends TestCase
 {
+
     use RefreshDatabase;
-    
+
     /** @test */
     public function a_channel_consists_of_threads()
     {
@@ -28,5 +30,14 @@ class ChannelTest extends TestCase
         $channel->archive();
 
         $this->assertTrue($channel->archived);
+    }
+
+    /** @test */
+    public function archived_channels_are_excluded_by_default()
+    {
+        create('App\Channel');
+        create('App\Channel', ['archived' => true]);
+
+        $this->assertEquals(1, Channel::count());
     }
 }
