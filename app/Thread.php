ae5e05387a2d1aee2dd77e4a24a7a7ce7f2c6534
AlekseyImashev <lesha.leshka@inbox.ru>
2018-07-12 10:15:42 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Thread.php
+++ app/Thread.php
@@ -188,7 +188,7 @@ class Thread extends Model
      */
     public function getIsSubscribedToAttribute()
     {
-        if (!auth()->id()) {
+        if (! auth()->id()) {
             return false;
         }
 
@@ -279,7 +279,7 @@ class Thread extends Model
      */
     public function hasBestReply()
     {
-        return !is_null($this->best_reply_id);
+        return ! is_null($this->best_reply_id);
     }
 
     /**
